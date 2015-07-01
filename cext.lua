aabc1c12fab279740d302aaa3feaf15bc7a5bd6f
Jared Allard <jaredallard@outlook.com>
2015-07-01 02:04:40 -0700

    cext: rewrite of most of the fs, dir support

--- cext.lua
+++ cext.lua
@@ -13,7 +13,7 @@ local cext = {
   ["version"] = 011,
   ["date"]    = 20150530,
   ["print"] = function(msg)
-    print(msg)
+    --print(msg)
     return nil -- no output
   end,
   ["fs"] = nil, -- assurance.
@@ -30,6 +30,20 @@ function string.split(s, delimiter)
     return result;
 end
 
+-- dump a table, for debug
+function dump(t, level)
+  level = level or 0
+  for i,v in pairs(t) do
+    io.write(string.rep('  ', level))
+    io.write(i..': ')
+    if type(v) == 'table' then
+      print ''
+      dump(v, level + 1)
+    else
+      print(tostring(v))
+    end
+  end
+end
 
 --[[
   Create a filesystem.
@@ -37,8 +51,6 @@ end
   @return {boolean} - success or failure
 ]]
 cext.createFS = function(this, location, size)
-  -- this.checkArgs(this, size)
-
   -- superblockHeader.
   local superblockHeader = this.version..",0,"..size
   local inodeAllocation = ""
@@ -74,12 +86,28 @@ cext.open = function(this, filesystem)
     ["path"]       = nil,
   }
 
+  if fs.exists(filesystem) ~= true then
+    return nil, "ERRNOTFOUND"
+  end
+
   -- load the raw filesystem.
   local iohandle     = fs.open(filesystem, "r")
   this.fs.superblock = iohandle.readLine() -- read the first line, the superblock.
-  this.fs.inodes     = iohandle.readLine() -- read the second line, the inodes.
+  this.fs.inodes_str     = iohandle.readLine() -- read the second line, the inodes.
   this.fs.data       = iohandle.readAll()  -- read the rest, this is data.
   iohandle.close()
+
+  local fs_length = ""
+  if this.fs.data ~= nil then
+    local l = string.split(this.fs.data, "\n")
+
+    local ii = 0
+    for i,v in pairs(l) do
+      ii = i;
+    end
+
+    fs_length = ii+3
+  end
 
   -- parse the superblock
   local superblockSplit = string.split(this.fs.superblock, ",")
@@ -91,13 +119,27 @@ cext.open = function(this, filesystem)
   this.fs.superblock.size = superblockSplit[3]
   this.fs.superblock.nextContents = superblockSplit[4]
 
-  this.print("superblock.version       = "..tostring(superblockSplit[1]))
-  this.print("superblock.inodes        = "..tostring(superblockSplit[2]))
-  this.print("superblock.size          = "..tostring(superblockSplit[3]))
-  this.print("superblock.nextContents  = "..tostring(superblockSplit[4]))
+  -- this.print("superblock.version       = "..tostring(superblockSplit[1]))
+  -- this.print("superblock.inodes        = "..tostring(superblockSplit[2]))
+  -- this.print("superblock.size          = "..tostring(superblockSplit[3]))
+  -- this.print("superblock.nextContents  = "..tostring(superblockSplit[4]))
+
+  -- DATA CHECK
+  if tostring(fs_length) ~= tostring(this.fs.superblock.nextContents) then
+    print("WARNING: DATA FOUND AFTER BLOCK, DATA LOSS MAY OCCUR")
+    print(fs_length.." ~= "..this.fs.superblock.nextContents)
+  end
 
   -- parse the inode line.
-  this.fs.inodes = this:parseInode(this.fs.inodes)
+  this.fs.inodes = {}
+  this.fs.inodes.isDir = "1"
+  local stat, err = this:parseInode(this.fs.inodes_str)
+
+  if err or stat == false then
+    print("cext: failed to load")
+    print("Err: "..tostring(err))
+    error("ERRFAILEDTOLOAD") -- critical error
+  end
 
   -- probably no errors, so set the path.
   this.fs.path = filesystem
@@ -126,20 +168,11 @@ cext.parseInode = function(this, serialized)
   for i, v in ipairs(filenames) do
     local inode = string.split(v, ",")
 
-    if tbl[inode[1]] == nil then
-      this.print("creating directory ")
-      tbl[inode[1]] = {}
-    end
-
     if inode[2] ~= nil then
-      -- add it to the table.
-      tbl[inode[1]][inode[2]] = {}
-      tbl[inode[1]][inode[2]].min = inode[3]
-      tbl[inode[1]][inode[2]].max = inode[4]
+      this.print("pre-inode: "..inode[2])
+      this:addInode("", inode[2], inode[3], inode[4], inode[5])
     end
   end
-
-  return tbl
 end
 
 --[[
@@ -147,37 +180,39 @@ end
 
   @return {boolean} - true on success
 ]]
-cext.addInode = function(this, filelocation, filename, min, max, typeof)
+cext.addInode = function(this, filelocation, filename, min, max, dir)
+
+  -- check if sub is object
   if this.fs.inodes[filelocation] == nil then
     this.fs.inodes[filelocation] = {}
   end
 
-  if typeof == nil then
-    typeof = "file"
+  this.print("cext: create inode for "..fs.combine(filelocation, filename))
+
+  function recurv_dir(this, d)
+    local s = ""
+    local dirs = string.split(d, "/")
+    for i,v in pairs(dirs) do
+      s = fs.combine(s, v)
+      this.print(d..": mkdir for "..v)
+      this:makeDir(s)
+    end
   end
 
-  local dlm = string.split(filelocation, "/")
-  local stack = ""
-  for i, v in ipairs(dlm) do
-    if i ~= 1 then
-      stack = stack .. "/" .. v
-    else
-      stack = stack .. v
-    end
+  local obj = {}
+  obj.min = min
+  obj.max = max
+  obj.filename = filename
+  obj.isDir = tostring(dir) -- is not a directory
+  obj.parent = filelocation
 
-    if this.fs.inodes[stack] == nil then
-      this.fs.inodes[stack] = {}
-    end
-
-    -- create the virtual link
-    this.fs.inodes[stack][v] = {}
-    this.fs.inodes[stack][v].dir = true
-    print(stack)
+  if filelocation == "" or tostring(dir) == "1" then -- root dir stuff.
+    this.fs.inodes[filename] = obj
+  else
+    this.print(filelocation.." +> "..filename)
+    this.fs.inodes[fs.combine(filelocation, filename)] = obj
+    recurv_dir(this, filelocation)
   end
-
-  this.fs.inodes[filelocation][filename] = {}
-  this.fs.inodes[filelocation][filename].min = min
-  this.fs.inodes[filelocation][filename].max = max
 
   return true
 end
@@ -190,37 +225,26 @@ end
 cext.serializeInode = function(this, object)
   local inode = ""
 
-  local function _wi(index, value, stack)
-    if type(value) ~= "table" then
-      return 2 -- entered inode territory
+  local function _wi(index, value)
+    if value.parent == nil then -- empty indexs
+      this.print("cext: invalid inode on <root>['"..index.."']")
+      return
     end
 
-    for i, v in pairs(value) do
-      local i = _wi(i, v, stack.."/"..i)
-
-      if i == 2 then
-        -- generate an inode
-        this.print("generate an inode for "..index)
-
-
-        -- ignore the virtual directory symlink
-        if value.dir ~= true then
-          inode = inode ..
-          fs.getDir(stack) ..     "," ..
-          index ..     "," ..
-          value.min .. "," ..
-          value.max .. "\\"
-        end
-
-        break
-      end
-    end
+    -- generate the inode string
+    inode = inode ..
+    value.parent ..     "," ..
+    index ..            "," ..
+    value.min ..        "," ..
+    value.max ..        "," ..
+    value.isDir ..      "\\"
   end
 
   for i, v in pairs(this.fs.inodes) do
     _wi(i, v, "")
   end
 
+  this.print(inode);
   return inode
 end
 
@@ -243,7 +267,7 @@ cext.close = function(this)
   fshandle.write(filesystem)
   fshandle.close()
 
-  this.print("cext: this.fs.data_table = "..tostring(this.fs.data_table))
+  this.print("cext: synced to disk.")
 
   return true
 end
@@ -270,22 +294,19 @@ cext.write = function(this, filename, data)
   this.print("write: fl="..fileLocation.." fn: "..fileName)
 
   for i, v in pairs(nl) do
-    max = max+1
-    local n = this.fs.superblock.nextContents+(i-1)
+    max = max+1 -- up by one
+    local n = this.fs.superblock.nextContents+(i-3)
 
     if i == #nl then
-      this.print("cext: write: i is nl len. call asm.")
+      -- this.print("cext: write: i is nl len. call asm.")
       this:writeToLine(n, v, true)
     else
       this:writeToLine(n, v)
     end
   end
 
-  -- print it out for now.
-  this.print(inodeObject)
-
   -- write the new inode to the actual line.
-  this:addInode(fileLocation, fileName, min, max)
+  this:addInode(fileLocation, fileName, min, max, "0")
 
   -- increment the amount of content lines
   this.fs.superblock.nextContents = max
@@ -308,7 +329,7 @@ cext.writeToLine = function(this, line, data, asm)
   this.fs.data_table[tonumber(line)] = data
 
   if asm == true then
-    this.print("cext: told to assemble the table into a string")
+    -- this.print("cext: told to assemble the table into a string")
     -- convert the table into a string.
     local d = ""
     for i,v in pairs(this.fs.data_table) do
@@ -332,15 +353,11 @@ end
   @return {string} - file contents, or nil on failure.
 ]]
 cext.read = function(this, file)
-  local fl = fs.getDir(file)
-  local fn = fs.getName(file)
-
-  if this.fs.inodes[fl][fn] == nil then
-    this.print("file not found")
-    return nil
+  if this:isDir(file) then
+    return nil, "ISDIR"
   end
 
-  local inode = this.fs.inodes[fl][fn]
+  local inode = this.fs.inodes[file]
 
   this.print("contents are on: "..inode.min.."-"..inode.max)
 
@@ -355,13 +372,13 @@ cext.readLines = function(this, linesep)
 
   local d = ""
   for i,v in pairs(nl) do
-    if i > min and i < max then
-      d = d..v.."\n"
+    if i > (min-2) and i < (max-2) or i == (min-2) then -- bad algo is bad
+      if (i+2) > (max-2) then
+        d = d..v
+      else
+        d = d..v.."\n"
+      end
     end
-  end
-
-  if d == "" then
-    return false
   end
 
   return d
@@ -374,17 +391,25 @@ end
 ]]
 cext.list = function(this, path)
   if this:isDir(path) == false then
-    return nil
+    print("Is not a DIR")
+    return nil, "ERRNOTADIR"
   end
 
-  if this.fs.inodes[path] == nil then -- doesn't exist
-    return nil
+  if this.fs.inodes == nil then
+    return nil, "ERRNOROOT"
   end
 
   local t = {}
-  for i,v in pairs(this.fs.inodes[path]) do
-    print(i)
-    table.insert(t, i)
+  local canonical_path = this.fs.inodes
+
+  if canonical_path == nil then -- doesn't exist
+    return nil, "ERRNOTEXIST"
+  end
+
+  for i,v in pairs(canonical_path) do
+    if v.parent == path then
+      table.insert(t, fs.getName(i))
+    end
   end
 
   return t
@@ -394,11 +419,38 @@ end
   Check if it is a directory.
 ]]
 cext.isDir = function(this, path)
-  if this.fs.inodes[path] == nil then -- doesn't exist
-    return false
-  elseif this.fs.inodes[path].max ~= nil then -- not a file
-    this.print("is a file")
-    return false
+  if path == "" then
+    canonical_path = this.fs.inodes
+  else
+    canonical_path = this.fs.inodes[path]
+  end
+
+  -- dump(canonical_path)
+
+  if canonical_path == nil then
+    return false, "ERRNOTEXIST"
+  elseif canonical_path.isDir ~= "1" then -- doesn't exist
+    return false, "WARNNOTDIRECTORY"
+  end
+
+  return true, "ISDIR"
+end
+
+--[[
+  Check if the file exists
+
+  @return true, or nil
+]]
+
+cext.exists = function(this, path)
+  if path == "" then
+    canonical_path = this.fs.inodes
+  else
+    canonical_path = this.fs.inodes[path]
+  end
+
+  if canonical_path == nil then
+    return nil
   end
 
   return true
@@ -410,40 +462,47 @@ end
   @return nil
 ]]
 cext.makeDir = function(this, path)
-  if this.fs.inodes[path] == nil then
-    this.fs.inodes[path] = nil
-  end
+  this.fs.inodes[path] = {}
+  this.fs.inodes[path].min = "0"
+  this.fs.inodes[path].max = "0"
+  this.fs.inodes[path].filename = fs.getName(path)
+  this.fs.inodes[path].isDir = "1" -- is not a directory
+  this.fs.inodes[path].parent = tostring(fs.getDir(path))
 
   return nil
 end
 
 
 -- TESTING
-fs.delete("test.fs")
-cext:createFS("test.fs", 1000)
-cext:open("test.fs")
-cext:write("nothing", "hacky fix")
+fs.delete("/cext/test.fs")
+cext:createFS("/cext/test.fs", 1000)
+cext:open("/cext/test.fs")
+cext:write("nothing", "ignore me data")
 cext:close()
-
-cext:open("test.fs")
-cext:write("a/dir/ccdocker", fs.open("ccdocker", "r").readAll())
+cext:open("/cext/test.fs")
+cext:write("a/dir/ccdocker", fs.open("/cext/ccdocker", "r").readAll())
 cext:close()
-
---cext:open("test.fs")
--- cext:read("ccdocker")
 
 if cext:isDir("") then
-  print("root is a dir")
+  print("root is a dir... good")
 else
-  print("root is not a dir, bad")
+  print("root is not a dir... bad")
 end
 
-if cext:isDir("ccdocker") then
+local c,r  = cext:isDir("a/dir/ccdocker")
+if c then
   print("regular file is a dir... bad")
 else
   print("ccdocker is not a dir... good!")
 end
 
-for i, v in pairs(cext:list("")) do
-  print(i..": "..v)
+local tbl, err = cext:list("a/dir")
+if err then
+  print("Err: "..err)
 end
+
+--[[for i,v in pairs(tbl) do
+  print(v)
+end]]
+
+cext:open("/cext/test.fs")
